29477a704cddb15a45142be27f330dbbbc7355c8
VSC Win <johndoe@example.com>
2025-08-04 13:50:14 +0200

    040825

--- AppShoping/ApplicationServices/Components/xmlImporter/IXmlImporter.cs
+++ AppShoping/ApplicationServices/Components/xmlImporter/IXmlImporter.cs
@@ -1,12 +1,4 @@
-﻿using AppShoping.ApplicationServices.Components.csvImporter;
-using AppShoping.ApplicationServices.Components.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AppShoping.ApplicationServices.Components.xmlImporter
+﻿namespace AppShoping.ApplicationServices.Components.xmlImporter
 {
     public interface IXmlImporter
     {
